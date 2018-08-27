4b567b456e16aaa3426cfba35d8b9606ecef0bb7
mriadh85 <mrislam1285@gmail.com>
2018-08-27 18:22:39 -0400

    java code file

---
+++ whileLoop.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int[] a = {2,3,4,5,6};
+        int i = 0;
+        do{
+            System.out.print(a[i]+"\n");
+            i++;
+        } while(i<a.length);
+    }
+}
