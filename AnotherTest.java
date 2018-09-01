0a8cf5eff3ff3a83bd00c89cf571705367cfd436
mriadh85 <mrislam1285@gmail.com>
2018-09-01 18:32:13 -0400

    enum practice code

---
+++ AnotherTest.java
@@ -0,0 +1,16 @@
+public class AnotherTest {
+    enum color{
+        RED, WHITE, BLUE
+    }
+
+    public static void main(String[] args) {
+        color[] ary1 = color.values();
+        for (color c: ary1
+             ) {
+            System.out.println(c +" is at "+c.ordinal());
+
+        }
+
+        System.out.println(color.valueOf("WHITE"));
+    }
+}
