92ecee281db3ce60bf6f1fb07b52e32355aa9e5d
TAREQ HASAN <akmhasan18@gmail.com>
2018-09-03 22:54:03 -0400

    stack file added

---
+++ UseOfStack.java
@@ -0,0 +1,19 @@
+package ClsPra082618;
+
+
+import java.util.Stack;
+
+public class UseOfStack {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack();
+        stack.push("NY");
+        stack.push("NJ");
+        stack.push("CT");
+
+        //System.out.println(stack.peek());
+        if(stack.peek().equalsIgnoreCase("CT")){
+            System.out.println(stack.pop());
+        }
+
+    }
+}
