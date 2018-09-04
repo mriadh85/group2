20faeba0965206e0cabe2fb37b77e257a3f82856
mriadh85 <mrislam1285@gmail.com>
2018-09-04 03:11:16 -0400

    use of string methods

---
+++ useOfStringMethods.java
@@ -0,0 +1,12 @@
+package stringmethod;
+
+public class useOfStringMethods {
+    public static void main(String[] args) {
+        String s1 = "My name is Riadhul and I'm studying selenium at PNT";
+        String s2 = "I want to be a software tester";
+
+
+
+
+    }
+}
