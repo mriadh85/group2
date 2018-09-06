d667a470a461e361009377f5c381662358a2ebaa
mriadh85 <mrislam1285@gmail.com>
2018-09-05 23:45:20 -0400

    String methods

---
+++ useOfMethodOfString.java
@@ -0,0 +1,17 @@
+package stringmethod;
+
+public class useOfMethods {
+    public static void main(String[] args) {
+        String s = "My name is Riadhul and I'm studying selenium at PNT";
+        String s1 = "I want to be a software tester";
+        String s2 = "i want to be a software Tester";
+
+        System.out.println(s.charAt(3));
+        System.out.println(s.codePointAt(3));
+        System.out.println(s.codePointBefore(3));
+        System.out.println(s.compareTo(s1));
+        
+
+
+    }
+}
